575a95e7b7ec2ceda5b6b9c41690dbb5492a0e65
talta <t.taltallc@gmail.com>
2017-01-02 13:36:25 -0500

    attempt to restrucuture the app into more modular code

--- main.js
+++ main.js
@@ -1,32 +1,61 @@
 var map;
 var APIKey = 'AIzaSyADZZ47Y-o54dJiJsxFJdeb2wnT5CSlkcQ';
-var geocoder = new google.maps.Geocoder;
+
 
 function displayMap(){
 
 	var loc = {lat: 42.877742, lng: -97.380979};
+
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 4,
 		center: loc
 	});
+	
 
-	map.addListener('click', function(event){
-		//removeMarker();
+	// document.getElementById('submit').addEventListener('click', function(){
+	// 	geocodeLatLng(geocoder, infoWindow, map);
+	// })
+	map.addListener('click', function(){
 		var marker = new google.maps.Marker({position: event.latLng, map: map});
-		
+		console.log(event.latLng); 
+		console.log(typeof(event.latLng));
+		geocodeLatLng(event);
+
+	})
+}
+
+function geocodeLatLng(event){
+
+		//removeMarker();
+		console.log(event.latLng); 
+		console.log(typeof(event.latLng));
 		var latAndLng = event.latLng.toString();
 		latAndLng = latAndLng.replace(/[{()}]/g, '');
 		latAndLng = latAndLng.replace(/\s/gi,'');
-		var contentString = '<div>' + event.latLng + '</div>';
+		var geocoder = new google.maps.Geocoder;
+		var contentString = ''
+		var infoWindow = new google.maps.InfoWindow;
+		// var latlngStr = input.split(',', 2);
+  //       var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+        
+        geocoder.geocode({'location': latAndLng}, function(results, status) {
+			if (status === 'OK'){
+					console.log(results);
+				}else {
+					window.alert('Geocoder failed due to '+status);
+				}
 
-		var infoWindow = new google.maps.InfoWindow({
-			content: contentString
-		});
+			// contentString = '<div>' + event.latLng + '</div>';
+
+			infoWindow = new google.maps.InfoWindow({
+				content: contentString
+			});
 
 		infoWindow.open(map, marker);
+		});
 
-		console.log(latAndLng);
-
+		// console.log(latAndLng);
+		// console.log('type of latlng '+typeof(latAndLng));
 		// getDataFromAPI(latAndLng);
 		// ///get info from API
 		// function getDataFromAPI(latAndLng){
@@ -46,19 +75,18 @@ function displayMap(){
 		// 	$.ajax(settings);
 		// 	console.log('ajax settings called');
 		// }
-		////alternative way to call the geocoder function
-		geocodeLatLng(geocoder, latAndLng, map, infoWindow);
-		function geocodeLatLng(geocoder, latAndLng, map, infoWindow){
-			console.log(geocoder);
-			geocoder.geocode({'location':latAndLng}, function(results, status){
-				if (status === 'OK'){
-					console.log(results);
-				}else {
-					window.alert('Geocoder failed due to '+status);
-				}
-			})
-		}
-	});
+		// //alternative way to call the geocoder function
+		// geocodeLatLng(geocoder, latAndLng, map, infoWindow);
+		
+		// 	console.log(geocoder);
+		// 	geocoder.geocode({'location':latAndLng}, function(results, status){
+		// 		if (status === 'OK'){
+		// 			console.log(results);
+		// 		}else {
+		// 			window.alert('Geocoder failed due to '+status);
+		// 		}
+		// 	})
+		// }
 
 }
 
