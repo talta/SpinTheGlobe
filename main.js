53f10b5194f7042482b229ce979f084af6a9e524
talta <t.taltallc@gmail.com>
2017-01-18 20:45:18 -0500

    removal of UNAPI potential connection

--- main.js
+++ main.js
@@ -17,19 +17,17 @@ function displayMap(){
 		var icon = {
 			url: 'treasure.svg',
 			size: new google.maps.Size(15, 15),
-			// origin: new google.maps.Point(0,0)
 		}
 		marker = new google.maps.Marker({
 			position: event.latLng, 
 			map: map,
 			icon: icon
-			// animation: google.maps.Animation.DROP,
 		});
 		var geocoder = new google.maps.Geocoder();
+		////parse GeoCoder into correct format:
 		var latAndLng = event.latLng.toString();
 		latAndLng = latAndLng.replace(/[{()}]/g, '');
 		var latlngStr = latAndLng.split(',', 2);
-		console.log('latlngstr '+latlngStr);
         var latLng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
 		
 
@@ -37,16 +35,9 @@ function displayMap(){
 
         geocoder.geocode({'location': latLng}, function(results, status) {
 			if (status === 'OK'){
-					console.log(results[0].formatted_address);
-					console.log(results[0].address_components);
-					/////find the address components here
-					
 					var address = results[0].formatted_address;
 					var message = address;
-					findCountryValue(results);
 					UpdateWindowMessage(message);
-					//var addressComponents = results[0].address_components
-					//FindTheCountry(addressComponents);
 				}else {
 					message = ('This body of water is unmarked, but it contains treasures.');
 					UpdateWindowMessage(message);
@@ -69,35 +60,7 @@ function displayMap(){
 				}
 			}
 		}
-		function findCountryValue(results){
-			var address = results[0].address_components;
-			for(var i = 0; i<=address.length-1; i++){
-				if (address[i].types[0] ==='country'){
-					var country = address[i].long_name;
-					console.log(country);
-					// UNAPIConnection(country);
-				}
-			};
-		}
-		function UNAPIConnection(country, HandleUNAPIData){
-			var settings = {
-				// url="http://api.undata-api.org/WHO/database/dataset/"+country"/records?app_id=8d589065&app_key=8bf907342cecae539da5d84b330e5542";
-				url: "http://api.undata-api.org/",
-				data: {
-					organization: "WHO",
-					database: "WHO Data",
-					dataset: "Median Consume Price Ratio of Selected Generic Medicines",
-					country: country,
-					records: "records",
-					app_id: "8d589065",
-					app_key: "8bf907342cecae539da5d84b330e5542"
-				}
-			};
-			$.ajax(settings);
-		}
-		function HandleUNAPIData(){
 
-		}
 	});
 }
 
