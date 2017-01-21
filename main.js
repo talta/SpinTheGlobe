c6de9d1f80dc1d7ce7df6768264e4be502148b3a
talta <t.taltallc@gmail.com>
2017-01-21 11:27:42 -0500

    updated structure, still with bugs

--- main.js
+++ main.js
@@ -1,66 +1,101 @@
 var map;
 var marker;
-
+var message;
+var icon;
+var bodyType;
 
 
 
 function displayMap(){
 	var loc = {lat: 0, lng: 0};
 
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 2,
 		center: loc
 	});
-	
+	locationListener();
+}
+
+function locationListener(){
 	map.addListener('click', function(event){
 		removeMarker();
-		var icon = {
-			url: 'treasure.svg',
-			size: new google.maps.Size(15, 15),
-		}
-		marker = new google.maps.Marker({
-			position: event.latLng, 
-			map: map,
-			icon: icon
-		});
-		var geocoder = new google.maps.Geocoder();
-		////parse GeoCoder into correct format:
-		var latAndLng = event.latLng.toString();
-		latAndLng = latAndLng.replace(/[{()}]/g, '');
-		var latlngStr = latAndLng.split(',', 2);
-        var latLng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-		
+		getAddress(event);
+		updateIconImage(bodyType);
+		createMarker(event);
+		UpdateWindowMessage(message);
+	});
+}
+	
 
 		
+function getAddress(event){
+	var geocoder = new google.maps.Geocoder();
+	////parse GeoCoder into correct format:
+	var latAndLng = event.latLng.toString();
+	latAndLng = latAndLng.replace(/[{()}]/g, '');
+	var latlngStr = latAndLng.split(',', 2);
+    var latLng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+	findAddress();
 
+	function findAddress(){
         geocoder.geocode({'location': latLng}, function(results, status) {
 			if (status === 'OK'){
 					var address = results[0].formatted_address;
-					var message = address;
-					UpdateWindowMessage(message);
+					message = address;
+					bodyType = 'land';
 				}else {
 					message = ('This body of water is unmarked, but it contains treasures.');
-					UpdateWindowMessage(message);
+					bodyType = 'water';
 				}
-
-
-			function UpdateWindowMessage(message){
-				var infoWindow = new google.maps.InfoWindow({
-					content: message
-				});
-				infoWindow.open(map, marker);
-			}
-
 		});
-		function removeMarker(){
-			if(typeof marker === 'object' && marker !== null){
-				if (map.getBounds().contains(marker.getPosition() ) ){
-					marker.setMap(null);
-				}
-			}
-		}
+	}
+}
 
+
+function createMarker(event){
+	marker = new google.maps.Marker({
+		position: event.latLng, 
+		map: map,
+		icon: icon
 	});
+}
+
+function updateIconImage(bodyType){
+	console.log('update icon image called');
+	if(bodyType === 'water'){
+		console.log('update water image called');
+		icon = {
+			url: 'treasure.svg',
+			size: new google.maps.Size (15,15)
+		}
+	}else{
+		icon = {
+			url: 'compass.svg',
+			size: new google.maps.Size (15,15)
+		}
+	}
+	console.log(icon.url);
+	console.log(bodyType);
+}
+
+function UpdateWindowMessage(message){
+	var infoWindow = new google.maps.InfoWindow({
+		content: message
+	});
+	infoWindow.open(map, marker);
+}
+
+function removeMarker(){
+	console.log('remove marker called');
+	if(typeof marker === 'object' && marker !== null){
+		if (map.getBounds().contains(marker.getPosition() ) ){
+			marker.setMap(null);
+		}
+	}
+}
+
+function clearBodyType(){
+	bodyType = '';
 }
 
 
